51df36b42d02ab97803602ec4e0fdd1d76f95ca9
Graham Ashton <graham@effectif.com>
2023-03-14 13:48:55 +0000

    Refactor to reduce dependency on external API
    By appending new config setting keys to the `@settings` class variable
    we were reaching into Nesta's internals in a manner that Nesta might not
    have been expecting.
    
    Also, in version 0.14.0, the `@settings` class var has gone away.
    
    The `Nesta::Config` class provides a `fetch()` method that we can use to
    read an arbitrary setting, so we can use that instead.
    
    There's also no need to write these values to Sinatra's global settings
    object, as they're defined in a scope that makes them available to the
    `before` and `after` blocks. So it's a bit less brittle (to unforeseen
    changes in Sinatra) to use local variables.

--- lib/nesta-plugin-cache/init.rb
+++ lib/nesta-plugin-cache/init.rb
@@ -2,39 +2,23 @@ module Nesta
   module Plugin
     module Cache
       module Helpers
-        # If your plugin needs any helper methods, add them here...
+        def page_cache_key(page, key, converter)
+          page.metadata(key).nil? ? nil : page.metadata(key).send(converter)
+        end
       end
-    end
-  end
-
-  class Config
-    @settings += %w[expires expires_type]
-  end
-
-  class Page
-    def expires
-      metadata('expires').nil? ? nil : metadata('expires').to_i
-    end
-
-    def expires_type
-      metadata('expires type').nil? ? nil : metadata('expires type').to_sym
     end
   end
 
   class App
     helpers Nesta::Plugin::Cache::Helpers
   
-    # Add the Nesta config entries to the Sinatra settings object
-    set :expires_time, Config.expires
-    set :expires_type, (Config.expires_type.nil? ?
-                        nil : Config.expires_type.to_sym)
+    expires_time = Nesta::Config.fetch('expires', nil)
+    expires_type = Nesta::Config.fetch('expires_type', 'public').to_sym
 
     # The default expires values
     before do
-      unless settings.expires_time.nil?
-        expires settings.expires_time,
-          settings.expires_type || :public,
-          :must_revalidate
+      unless expires_time.nil?
+        expires(expires_time, expires_type, :must_revalidate)
       end
     end
 
@@ -42,10 +26,13 @@ module Nesta
       # Check that this is a regular page (ie not the sitemap, atom feed, an
       # attachement or a CSS stylesheet): these don't have metadata; and check
       # that the page defined a per-page expires directive.
-      unless @page.nil? || @page.expires.nil?
-        expires @page.expires,
-          @page.expires_type || settings.expires_type || :public,
-          :must_revalidate
+      unless @page.nil?
+        page_expires_time = page_cache_key(@page, 'expires', :to_i)
+        page_expires_type = page_cache_key(@page, 'expires type', :to_sym)
+        unless page_expires_time.nil?
+          type = page_expires_type || expires_type
+          expires(page_expires_time, type, :must_revalidate)
+        end
       end
     end
   end
